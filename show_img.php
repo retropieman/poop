272f67839ff2ab0f3beb3a0ab514c32320f00aaf
Witherman17 <31626399+retropieman@users.noreply.github.com>
2019-04-11 19:47:02 -0600

    Add files via upload

---
+++ show_img.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+Owner: <?php echo $_GET['name']; ?> <br/>
+Description: <?php echo $_GET['desc']; ?> <br/>
+<img src="<?php echo $_GET['linkto']; ?>" /><br/>
+<a href="index.html">Go home</a>
+
+</body>
+</html>
